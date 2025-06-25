72a019ee7404cf7f3285c4b0e0836d6f1334654d
sahi <123293234+sahichilakamari@users.noreply.github.com>
2025-06-25 15:08:14 +0530

    Add files via upload

---
+++ backend/parser/resume_parser.py
@@ -0,0 +1,8 @@
+import fitz  # PyMuPDF
+
+def extract_resume_text(file):
+    doc = fitz.open(stream=file.read(), filetype="pdf")
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    return text.lower()
