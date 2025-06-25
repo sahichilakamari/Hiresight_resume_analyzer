09a43f84c2400fd2372eb31191ddea3043d8ecf3
sahi <123293234+sahichilakamari@users.noreply.github.com>
2025-06-25 15:07:31 +0530

    Delete backend/parser directory

--- backend/parser/job_parser.py
+++
@@ -1,3 +0,0 @@
-def extract_jd_text(file):
-    text = file.read().decode("utf-8")
-    return text.lower()
--- backend/parser/resume_parser.py
+++
@@ -1,8 +0,0 @@
-import fitz  # PyMuPDF
-
-def extract_resume_text(file):
-    doc = fitz.open(stream=file.read(), filetype="pdf")
-    text = ""
-    for page in doc:
-        text += page.get_text()
-    return text.lower()
